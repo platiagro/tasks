497c6828ed891998996fbbd337a06d7286843318
lladeira <lladeira@AB021647.local>
2020-12-30 11:22:55 -0300

    Change yolo name to follow cv pattern.

---
+++ tests/test_cv_yolo.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+import uuid
+
+import papermill
+
+from tests import datasets
+
+EXPERIMENT_ID = str(uuid.uuid4())
+OPERATOR_ID = str(uuid.uuid4())
+RUN_ID = str(uuid.uuid4())
+
+
+class TestCVYOLO(unittest.TestCase):
+
+    def setUp(self):
+        # Set environment variables needed to run notebooks
+        os.environ["EXPERIMENT_ID"] = EXPERIMENT_ID
+        os.environ["OPERATOR_ID"] = OPERATOR_ID
+        os.environ["RUN_ID"] = RUN_ID
+
+        datasets.ocr()
+
+    def tearDown(self):
+        datasets.clean()
+
+    def test_experiment_ocr_output_image(self):
+        notebook_path = "tasks/cv-yolo/Experiment.ipynb"
+
+        papermill.execute_notebook(
+            notebook_path,
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/ocr_dataset.zip",
+            ),
+        )
+
+
