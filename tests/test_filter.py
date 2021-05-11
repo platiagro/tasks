d657b605aecdc1c7d1016965d5ca2205cb891c0b
Gabriela C. Vechini <32876443+gvechini@users.noreply.github.com>
2021-05-11 08:41:55 -0300

    Add test dataset with target
    filter selection needs dataset with target for test

---
+++ tests/test_filter.py
@@ -0,0 +1,97 @@
+import os
+import unittest
+import uuid
+
+import papermill
+
+from tests import datasets, server
+
+EXPERIMENT_ID = str(uuid.uuid4())
+OPERATOR_ID = str(uuid.uuid4())
+RUN_ID = str(uuid.uuid4())
+
+
+class TestFilterSelection(unittest.TestCase):
+
+    def setUp(self):
+        # Set environment variables needed to run notebooks
+        os.environ["EXPERIMENT_ID"] = EXPERIMENT_ID
+        os.environ["OPERATOR_ID"] = OPERATOR_ID
+        os.environ["RUN_ID"] = RUN_ID
+
+        datasets.iris()
+        datasets.titanic()
+        datasets.hotel_bookings()
+
+        os.chdir("tasks/filter-selection")
+
+    def tearDown(self):
+        datasets.clean()
+        os.chdir("../../")
+
+    def test_experiment_iris(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/iris.csv",
+                features_to_filter=["SepalLengthCm"]
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+        data = datasets.iris_testdata_full()
+        with server.Server() as s:
+            response = s.test(data=data)
+
+        ndarray = response["ndarray"]
+        self.assertEqual(len(ndarray[0]), 4)  # 5 features - 1 removed
+        self.assertEqual(len(names), 4)
+
+    def test_experiment_titanic(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/titanic.csv",
+                features_to_filter=["Name"]
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+        data = datasets.titanic_testdata_full()
+        with server.Server() as s:
+            response = s.test(data=data)
+
+        ndarray = response["ndarray"]
+        self.assertEqual(len(ndarray[0]), 11)  # 12 features - 1 removed
+        self.assertEqual(len(names), 11)
+
+    def test_experiment_hotel_bookings(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/hotel_bookings.csv",
+                features_to_filter=["reservation_status_date", "arrival_date_year"]
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+        data = datasets.hotel_bookings_testdata_full()
+        with server.Server() as s:
+            response = s.test(data=data)
+
+        ndarray = response["ndarray"]
+        self.assertEqual(len(ndarray[0]), 30)  # 32 features - 2 removed
+        self.assertEqual(len(names), 30)
+
