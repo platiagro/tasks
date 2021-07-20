9a2740104bf255463c0fba9e646c41a48b3c91a6
Lucas Nunes Sequeira <lsequeira@cpqd.com.br>
2021-07-20 11:36:48 -0300

    Update draws.py
    Update bounding box image drawing to better resolution.

--- tasks/cv-mtcnn-face-detection/draws.py
+++ tasks/cv-mtcnn-face-detection/draws.py
@@ -1,78 +1,88 @@
-# Import Dependencies
-from PIL import Image
-import cv2
+import colorsys
 import numpy as np
+import cv2
+from unidecode import unidecode
 
+# Inspired by https://github.com/hhk7734/tensorflow-yolov4
 
-def draw_bboxes(img_arr, bboxes, labels = None, probs = None):
-    '''
-    Parameters
-    ----------
-    img_arr : np.ndarray
-        A array containing the data of a 3D image
+_MAX_CLASSES = 14 * 6
+_HSV = [(x / _MAX_CLASSES, 1.0, 1.0) for x in range(int(_MAX_CLASSES * 1.2))]
+_COLORS = [colorsys.hsv_to_rgb(*x) for x in _HSV]
+_COLORS = [(int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)) for x in _COLORS]
+_BBOX_COLORS = []
+for i in range(_MAX_CLASSES):
+    # 0 14 28 42 56 70 1 15 29 43 57 71 2 ...
+    _BBOX_COLORS.append(_COLORS[14 * (i % 6) + (i // 6)])
 
-    bboxes : np.ndarray, list
-        A array of bboxes for each bbox found in the image
+def draw_bboxes(
+    image: np.ndarray, bboxes: np.ndarray, probs: np.ndarray, names: np.ndarray
+):
+    """
+    @parma `image`:  Dim(height, width, channel)
+    @parma `bboxes`
+        Dim(-1, (x_min, y_min, x_max, y_max))
+    @parma `probs`
+        Dim(-1,)
+    @parma `names`
+        Dim(-1,)
+    @return drawn_image
+    Usage:
+        image = yolo.draw_bboxes(image, bboxes, probs, names)
+    """
+    height, width, _ = image.shape
+    image = np.copy(image)
+    name_ids = np.unique(names)
 
-    labels : np.ndarray, list
-        A array of labels for each bbox found in the image
-        
-    probs : np.ndarray, list
-        A array of probabilities for each bbox found in the image
-
-    Returns
-    -------
-    PIL.Image
-        Returns a Image containing the bbox drawn on it, and its
-        associated labels and probabilities.
-    '''
-    
-    # Preset colors
-    box_color  = (255,0,0) # red
-    text_color = (0, 0, 0) # black
-    
-    # Image dimension size (should be square)
-    img_size   = img_arr.shape[0]
-    
+    # Draw bboxes
     for bbox_id, bbox in enumerate(bboxes):
         
-        # Bbox
-        thickness = 2
+        left = int(bbox[0]) # x_min
+        top = int(bbox[1]) # y_min
+        right = int(bbox[2]) # x_max
+        bottom = int(bbox[3]) # y_max
+
+        font_size = 0.4
+        font_thickness = 1
         
-        # Retrieve the start and end point
-        start_point = bbox[:2].astype(np.int).tolist()
-        end_point   = bbox[2:].astype(np.int).tolist()
-        
-        # Draw
-        img_arr = cv2.rectangle(img_arr, tuple(start_point), tuple(end_point), box_color, thickness)
-        
-        # Labels and probs (Text)
-        if labels is None and probs is None: continue
-            
-        if labels is not None:
-            label = labels[bbox_id]
-        if probs is not None:
-            prob = probs[bbox_id]
-        
+        # find name id, prob and set color
+        name_id = np.where(np.array(name_ids) == names[bbox_id])[0][0]
+        prob = probs[bbox_id]
+        color = _BBOX_COLORS[name_id%_MAX_CLASSES]
+
+        # Get text size
+        bbox_text = "{}: {:.1%}".format(names[bbox_id], prob)
+        t_w, t_h = cv2.getTextSize(bbox_text, 0, font_size, font_thickness)[0]
+        t_h += 3
+
+        # Draw box
+        if top < t_h:
+            top = t_h
+        if left < 1:
+            left = 1
+        if bottom >= height:
+            bottom = height - 1
+        if right >= width:
+            right = width - 1
+
+        cv2.rectangle(image, (left, top), (right, bottom), color, 1)
+
         # Draw text box
-        start_point[1] -= 20
-        start_point[0] -= 1
-        end_point[1]    = start_point[1] + 20
-        end_point[0]   += 1
-        
-        img_arr = cv2.rectangle(img_arr, tuple(start_point), tuple(end_point), box_color, -1)
-        
+        cv2.rectangle(image, (left, top), (left + t_w, top - t_h), color, -1)
+
         # Draw text
-        font      = cv2.FONT_HERSHEY_SIMPLEX
-        pos       = [start_point[0]+5, end_point[1]-5]
-        fontScale = img_size/2048
-        thickness = 1
-        
-        if labels is not None and probs is not None:
-            img_arr = cv2.putText(img_arr, f'{label}: {prob:.4f}', tuple(pos) , font, fontScale, text_color, thickness, cv2.LINE_AA)
-        elif labels is not None:
-            img_arr = cv2.putText(img_arr, f'{label}', tuple(pos) , font, fontScale, text_color, thickness, cv2.LINE_AA)
-        else:
-            img_arr = cv2.putText(img_arr, f'{prob:.4f}', tuple(pos) , font, fontScale, text_color, thickness, cv2.LINE_AA)
-    
-    return Image.fromarray(img_arr)
+        cv2.putText(
+            image,
+            unidecode(bbox_text), # OpenCV does not handle ~, ^, ´, etc..
+            (left, top - 2),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            font_size,
+            (
+                255 - color[0],
+                255 - color[1],
+                255 - color[2],
+            ),
+            font_thickness,
+            lineType=cv2.LINE_AA,
+        )
+
+    return image
