b6172a73873967bc4bd3f473da24e06a6caceb84
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2021-07-21 17:16:08 -0300

    Added tests

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,21 +1,23 @@
 import unittest
 import sys
 import json
-
 # we need import this way just because python can handle well '-' character in import
 sys.path.append("./init-job")
 
-import notebook 
+import notebook
+
+TEST_NOTEBOOK_FILEPATH = 'tests/resources/parameters.ipynb'
+
 
 class TestParameters(unittest.TestCase):
 
     def test_parameters_reading(self):
-        a = notebook.parse_parameters('tests/resources/parameters.ipynb')
-        
-        j = json.loads( 'tests/resources/expected_parameters.json')
-        print(j)        
-        
-        
-        print(a)
+        file = open('tests/resources/expected_parameters.json')
 
-       
+        result = notebook.parse_parameters(TEST_NOTEBOOK_FILEPATH)
+        expected = json.load(file)
+
+        # variables 'result' and 'expected' are list of dictionaries not a dictionary itself!
+        self.assertListEqual(expected, result)
+
+        file.close()
