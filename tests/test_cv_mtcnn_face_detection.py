bd0853cd691095a129d6bec22af63adeb41a7e6b
lunseq <43300925+lucasns97@users.noreply.github.com>
2021-07-22 14:50:53 -0300

    Update test_cv_mtcnn_face_detection.py
    Add deployment tests

--- tests/test_cv_mtcnn_face_detection.py
+++ tests/test_cv_mtcnn_face_detection.py
@@ -1,10 +1,11 @@
+import logging
 import os
 import unittest
 import uuid
 
 import papermill
 
-from tests import datasets
+from tests import datasets, server
 
 EXPERIMENT_ID = str(uuid.uuid4())
 OPERATOR_ID = str(uuid.uuid4())
@@ -27,11 +28,41 @@ class TestMTCNNFaceDetection(unittest.TestCase):
         datasets.clean()
         os.chdir("../../")
 
-    def test_experiment_ocr_output_image(self):
+    def test_experiment_face_detection_output_image(self):
         papermill.execute_notebook(
             "Experiment.ipynb",
             "/dev/null",
             parameters=dict(
                 dataset="/tmp/data/football_teams.zip",
+                image_size=64,
+                margin=5,
+                min_face_size=10,
+                factor=0.709,
+                keep_all=True,
+                device="cpu",
+                seed=7,
+                inference_batch_size=2,
+                input_square_transformation_size=16,
             ),
         )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+
+        data = datasets.image_testdata(kind='people', ext='png')
+        
+        with server.Server() as s:
+            response = s.test(data=data, timeout=30)
+        
+        if 'tensor' in response.keys():
+            tensor_shape = response["tensor"]['shape']
+            self.assertEqual(tensor_shape, [1, 5]) # 1 output, 5 features
+
+        else: # is a ndarray
+            ndarray = response["ndarray"]
+            self.assertEqual(len(ndarray[0]), 5) # 5 features
+        
+        names = response["names"]
+        self.assertEqual(len(names), 5) # 5 feature names
