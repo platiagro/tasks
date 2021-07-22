fbcbfd6c5bb082c2db00ce42d6ebf582c9b85935
lunseq <43300925+lucasns97@users.noreply.github.com>
2021-07-22 17:12:59 -0300

    Update test_cv_mtcnn_face_detection.py
    Updated tests for Face Detection: cuda, default, has face and hasn't faces

--- tests/test_cv_mtcnn_face_detection.py
+++ tests/test_cv_mtcnn_face_detection.py
@@ -4,6 +4,7 @@ import unittest
 import uuid
 
 import papermill
+import numpy as np
 
 from tests import datasets, server
 
@@ -28,7 +29,7 @@ class TestMTCNNFaceDetection(unittest.TestCase):
         datasets.clean()
         os.chdir("../../")
 
-    def test_experiment_face_detection_output_image(self):
+    def test_experiment_face_detection_with_people(self):
         papermill.execute_notebook(
             "Experiment.ipynb",
             "/dev/null",
@@ -42,7 +43,7 @@ class TestMTCNNFaceDetection(unittest.TestCase):
                 device="cpu",
                 seed=7,
                 inference_batch_size=2,
-                input_square_transformation_size=16,
+                input_square_transformation_size=128,
             ),
         )
 
@@ -51,17 +52,135 @@ class TestMTCNNFaceDetection(unittest.TestCase):
             "/dev/null",
         )
 
-        data = datasets.image_testdata(kind='people', ext='png')
-        
+        for ext in ['png', 'jpg']:
+
+            data = datasets.image_testdata(kind='people', ext=ext)
+
+            with server.Server() as s:
+                response = s.test(data=data, timeout=10)
+            
+            if 'tensor' in response.keys():
+                tensor_shape = response["tensor"]['shape']
+                tensor_values = response["tensor"]['values']
+
+                self.assertEqual(tensor_shape[1], 5) # outputs 5 features
+                self.assertEqual(np.isnan(tensor_values[0]), False) # output is not empty
+
+            else: # is a ndarray
+                ndarray = response["ndarray"]
+
+                self.assertEqual(len(ndarray[0]), 5) # 5 features
+                self.assertEqual(np.isnan(ndarray[0]), False) # output is not empty
+            
+            names = response["names"]
+            self.assertEqual(len(names), 5) # 5 feature names
+    
+    def test_experiment_face_detection_without_people(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/football_teams.zip",
+                image_size=64,
+                margin=5,
+                min_face_size=10,
+                factor=0.709,
+                keep_all=True,
+                device="cpu",
+                seed=7,
+                inference_batch_size=2,
+                input_square_transformation_size=128,
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+
+        for ext in ['png', 'jpg']:
+
+            data = datasets.image_testdata(kind='objects', ext=ext)
+
+            with server.Server() as s:
+                response = s.test(data=data, timeout=10)
+            
+            if 'tensor' in response.keys():
+                tensor_shape = response["tensor"]['shape']
+                tensor_values = response["tensor"]['values']
+
+                self.assertEqual(tensor_shape[1], 5) # outputs 5 features
+                self.assertEqual(np.isnan(tensor_values[0]), True) # output is empty
+
+            else: # is a ndarray
+                ndarray = response["ndarray"]
+
+                self.assertEqual(len(ndarray[0]), 5) # 5 features
+                self.assertEqual(np.isnan(ndarray[0]), True) # output is empty
+            
+            names = response["names"]
+            self.assertEqual(len(names), 5) # 5 feature names
+
+    def test_experiment_face_detection_default_params(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/football_teams.zip",
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+
+        data = datasets.image_testdata(kind='people', ext='jpg')
+
         with server.Server() as s:
-            response = s.test(data=data, timeout=30)
+            response = s.test(data=data, timeout=10)
         
         if 'tensor' in response.keys():
             tensor_shape = response["tensor"]['shape']
-            self.assertEqual(tensor_shape, [1, 5]) # 1 output, 5 features
+
+            self.assertEqual(tensor_shape[1], 5) # output 5 features
 
         else: # is a ndarray
             ndarray = response["ndarray"]
+
+            self.assertEqual(len(ndarray[0]), 5) # 5 features
+        
+        names = response["names"]
+        self.assertEqual(len(names), 5) # 5 feature names
+
+    def test_experiment_face_detection_cuda(self):
+        papermill.execute_notebook(
+            "Experiment.ipynb",
+            "/dev/null",
+            parameters=dict(
+                dataset="/tmp/data/football_teams.zip",
+                device="cuda",
+            ),
+        )
+
+        papermill.execute_notebook(
+            "Deployment.ipynb",
+            "/dev/null",
+        )
+
+        data = datasets.image_testdata(kind='people', ext='jpg')
+
+        with server.Server() as s:
+            response = s.test(data=data, timeout=10)
+        
+        if 'tensor' in response.keys():
+            tensor_shape = response["tensor"]['shape']
+
+            self.assertEqual(tensor_shape[1], 5) # output 5 features
+
+        else: # is a ndarray
+            ndarray = response["ndarray"]
+
             self.assertEqual(len(ndarray[0]), 5) # 5 features
         
         names = response["names"]
