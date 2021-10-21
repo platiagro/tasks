e240e72d44940f6063341afb432f48a487d62cf1
lunseq <43300925+lucasns97@users.noreply.github.com>
2021-10-21 14:38:41 -0300

    Upload Retriever Component
    Upload Retriever Component (notebooks and py scripts)

---
+++ tasks/retriever/models/bm25.py
@@ -0,0 +1,92 @@
+# Transformers
+from rank_bm25 import BM25Okapi
+import numpy as np
+import torch.nn as nn
+
+# Typing
+from typing import List
+
+# Instantiate class
+class BM25(nn.Module):
+    """Compute sentence similarity between sentences using the BM25"""
+    def __init__(self, **kwargs):
+
+        super(BM25, self).__init__()
+
+        # Model hparams
+        self.hparams = {
+            'k1': kwargs.get('k1', 2.16),
+            'b': kwargs.get('b', 0.61)
+        }
+
+    def __name__(self):
+        return "BM25"
+
+    def _tokenize(self, corpus: List[str]) -> List[List[str]]:
+        return [doc.split(" ") for doc in corpus]
+
+    def _fit(self, sentences: List[str]):
+        """Fit the model
+            
+            Args:
+                sentences: List of sentences
+        """
+
+        # Fit model
+        self.model = BM25Okapi(self._tokenize(sentences),
+                               k1=self.hparams['k1'],
+                               b=self.hparams['b'])
+
+    def _calculate_similarities(self, hypothesis_sentences: List[str], reference_sentece: str) -> float:
+        """Calculate similarities between hypothesis and reference sentence"""
+
+        # Fit model
+        self._fit(hypothesis_sentences)
+
+        # Tokenize reference
+        reference_tokenized = self._tokenize([reference_sentece])[0]
+
+        # Similarities
+        similarities = np.array(self.model.get_scores(reference_tokenized))
+
+        return similarities
+
+    def forward(self, batch_hypothesis_sentences: List[List[str]], batch_reference_sentece: List[str]) -> float:
+        """Calculate similarity between batch of sentences
+            
+            >>> batch_hypothesis_sentences = [['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas'], ['Esta é uma sentença de exemplo 2']]
+            >>> batch_reference_sentece = ['Esta sentença é um exemplo', 'Esta é a referencia do exemplo 2']
+            >>> model = BM25()
+            >>> model(batch_hypothesis_sentences, batch_reference_sentece)
+                [[0.0,  0.0], [-1.09861229]]
+
+            Args:
+                batch_hypothesis_sentences: Batch of list of hypothesis sentences
+                batch_reference_sentece: Batch of reference sentences
+            Returns:
+                A batch of similarities
+        """
+
+        # Assure batches have same length
+        assert len(batch_hypothesis_sentences) == len(batch_reference_sentece), "Hypothesis batch and reference batch must have same length."
+
+        # Batch similarities to be calculated
+        batch_similarities = []
+
+        for hypothesis_sentences, reference_sentece in zip(batch_hypothesis_sentences, batch_reference_sentece):
+
+            # Calculate reciprocal rank
+            similarities = self._calculate_similarities(hypothesis_sentences, reference_sentece)
+
+            # Add to Batch similarities
+            batch_similarities.append(similarities)
+
+        return batch_similarities
+
+
+# Sentences we want sentence embeddings for
+# batch_hypothesis_sentences = [['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas'], ['Esta é uma sentença de exemplo 2']]
+# batch_reference_sentece = ['Esta sentença é um exemplo', 'Esta é a referencia do exemplo 2']
+
+# sent_sim = BM25()
+# print(sent_sim(batch_hypothesis_sentences, batch_reference_sentece))
---
+++ tasks/retriever/models/word2vec.py
@@ -0,0 +1,120 @@
+# Transformers
+import spacy
+import numpy as np
+import torch.nn as nn
+
+# Typing
+from typing import List
+
+# Instantiate class
+class Word2Vec(nn.Module):
+    """Compute sentence similarity between sentences using the spacy Word2Vec representation"""
+    def __init__(self, **kwargs):
+
+        super(Word2Vec, self).__init__()
+
+        # Get hparams
+        model_size = kwargs.get("model_size", "lg")
+
+        # Assure model size is valid
+        assert model_size in ["sm", "md", "lg"], "Model size must be either 'sm', 'md' or 'lg'."
+
+        # Save model name
+        self.model_name = f'pt_core_news_{model_size}'
+
+        # Initialize model
+        self._init_model()
+
+    def __name__(self):
+        return "Word2Vec"
+    
+    def _init_model(self):
+        """Initialize model"""
+
+        # Load model
+        self.model = spacy.load(self.model_name)
+
+        # Precomputed docs
+        self._precomputed_docs = {}
+
+    def _save_docs(self, sentences: List[str], docs):
+        """Save docs to internal dict"""
+
+        for i, sentence in enumerate(sentences):
+            self._precomputed_docs[sentence] = docs[i]
+
+    def _get_precomputed_docs(self, sentences: List[str]):
+        """Get precomputed docs from internal dict"""
+
+        # Get precomputed embeddings
+        docs = []
+        for sentence in sentences:
+            docs.append(self._precomputed_docs[sentence])
+
+        return docs
+
+    def _calculate_similarities(self, hypothesis_sentences: List[str], reference_sentece: str) -> float:
+        """Calculate similarities between hypothesis and reference sentence"""
+
+        # Verify if all hypothesis sentences are already precomputed
+        precomputed = False
+        for hypothesis_sentence in hypothesis_sentences:
+            if hypothesis_sentence not in self._precomputed_docs.keys():
+                precomputed = False
+                break
+            else:
+                precomputed = True
+        
+        # Retrieve the document representations for hypothesis sentences and reference sentence
+        if not precomputed:
+            hypothesis_docs = [self.model(hypothesis_sentence) for hypothesis_sentence in hypothesis_sentences]
+            self._save_docs(hypothesis_sentences, hypothesis_docs)
+        else:
+            hypothesis_docs = self._get_precomputed_docs(hypothesis_sentences)
+            
+        reference_doc = self.model(reference_sentece)
+
+        # Similarities
+        similarities = np.array([reference_doc.similarity(hypothesis_doc) for hypothesis_doc in hypothesis_docs])
+
+        return similarities
+
+    def forward(self, batch_hypothesis_sentences: List[List[str]], batch_reference_sentece: List[str]) -> List:
+        """Calculate similarity between batch of sentences
+            
+            >>> batch_hypothesis_sentences = [['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas'], ['Esta é uma sentença de exemplo 2']]
+            >>> batch_reference_sentece = ['Esta sentença é um exemplo', 'Esta é a referencia do exemplo 2']
+            >>> model = Word2Vec()
+            >>> model(batch_hypothesis_sentences, batch_reference_sentece)
+                [[0.79393229,  0.15021144], [0.86997343]]
+
+            Args:
+                batch_hypothesis_sentences: Batch of list of hypothesis sentences
+                batch_reference_sentece: Batch of reference sentences
+            Returns:
+                A batch of similarities
+        """
+
+        # Assure batches have same length
+        assert len(batch_hypothesis_sentences) == len(batch_reference_sentece), "Hypothesis batch and reference batch must have same length."
+
+        # Batch similarities to be calculated
+        batch_similarities = []
+
+        for hypothesis_sentences, reference_sentece in zip(batch_hypothesis_sentences, batch_reference_sentece):
+            
+            # Calculate similarity
+            similarities = self._calculate_similarities(hypothesis_sentences, reference_sentece)
+
+            # Add to Batch similarities
+            batch_similarities.append(similarities)
+
+        return batch_similarities
+
+
+# # Sentences we want sentence embeddings for
+# batch_hypothesis_sentences = [['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas'], ['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas'], ['Esta é uma sentença de exemplo', 'Todas as sentenças são cobertas']]
+# batch_reference_sentece = ['Esta sentença é um exemplo', 'Esta é a referencia do exemplo 2', 'Esta é a referencia do exemplo 3']
+
+# sent_sim = Word2Vec(model_size="md")
+# print(sent_sim(batch_hypothesis_sentences, batch_reference_sentece))
