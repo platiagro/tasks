cd552d096595b4d084dbded2d260d10fa59abcab
vitoryeso <vitoryeso@outlook.com>
2021-10-22 18:25:30 -0300

    add pdf-extractor component and tests

--- tasks/pdf-extractor/Model.py
+++ tasks/pdf-extractor/Model.py
@@ -5,7 +5,8 @@ import numpy as np
 import base64
 import cv2
 import logging
-from pdf_extractor import PDFExtractor, read_memory
+import io
+from pdf_extractor import PDFExtractor, read_memory, read_file
 
 class Model:
     def __init__(self):
@@ -15,6 +16,12 @@ class Model:
         artifacts = joblib.load("/tmp/data/pdf_extractor.joblib")
         self.extractor_parameters = artifacts["extractor_parameters"]
         
+        if self.extractor_parameters["initial_page"] != None: 
+            self.extractor_parameters["initial_page"] -= 1;
+
+        if self.extractor_parameters["final_page"] != None: 
+            self.extractor_parameters["final_page"] -= 1;
+        
         self.loaded = True
         print("Loaded model")
 
@@ -22,7 +29,6 @@ class Model:
         if not self.loaded:
             self.load()
         
-        # check if data is bytes
         if isinstance(X, bytes):
             result = read_memory(X,
                                 extract = self.extractor_parameters["extract"],
@@ -30,16 +36,33 @@ class Model:
                                 text_filter_end = self.extractor_parameters["text_filter_end"],
                                 initial_page = self.extractor_parameters["initial_page"],
                                 final_page = self.extractor_parameters["initial_page"])
+        else:
+            return ""
                                         
         if self.extractor_parameters["extract"] == "text":
+            #return np.array([1,2]);
             return result
     
         elif self.extractor_parameters["extract"] == "figures":
-            for img in result:
-                img = np.array(img)
-            return np.stack(result, axis=0)
+            result_bytes = []
+            if len(result) > 0:
+                for img in result:
+                    buff = io.BytesIO()
+                    img.save(buff, format="JPEG")
+                    result_bytes.append(buff.getvalue().decode("latin1"))
+
+                return result_bytes;
+            else:
+                return result
     
         elif self.extractor_parameters["extract"] == "prints":
-            for img in result:
-                img = np.array(img)
-            return np.stack(result, axis=0)
+            result_bytes = []
+            if len(result) > 0:
+                for img in result:
+                    buff = io.BytesIO()
+                    img.save(buff, format="JPEG")
+                    result_bytes.append(buff.getvalue().decode("latin1"))
+
+                return result_bytes;
+            else:
+                return result
